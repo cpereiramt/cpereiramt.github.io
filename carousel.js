b79a7afe44bd168d5d5529f73acc0974f84d9205
Clayton Pereira <cpereiramt@gmail.com>
2020-10-09 16:49:32 -0400

    Salvando alteraçoes no seçao de projetos

--- carousel.js
+++ carousel.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, {padding: 100 });
+    var instances = M.Carousel.init(elems, {fullwidth: true});
   });
