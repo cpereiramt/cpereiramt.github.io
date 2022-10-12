8d409fa0e8b5f5111891e88e9ab0f4e5bf2af834
cpereiramt <cpereiramt@gmail.com>
2022-10-12 16:02:01 -0400

    fix: fixing errors

--- data/applicationState.js
+++ data/applicationState.js
@@ -1,72 +1,16 @@
-import * as projectType from "/utils/projectsTypes.js";
-import * as technologiesTypes from "/utils/technologiesTypes.js";
 import * as applicationPoint from "/utils/applicationPoint.js";
+import projectData from "/data/projectsData.js";
+
 const state = {
   projects: [],
   projectsType: [],
   technologiesUsed: [],
+  typeOfProjectSelected: "default",
   view: applicationPoint.NOT_STARTED, // application actual point
 };
 
 // Add project info Here
-state.projects = [
-  {
-    projectID: 1,
-    projectTitle: "Vigilante 8 2 Page tribute",
-    projectURL: "https://claytonpereira.com/Proj_bloco3_vigilante_page/",
-    projectOverview:
-      " Vigilante 8: 2nd Offense is a vehicular combat game developed by Luxoflux and published by Activision, it was first released in 1999",
-    projectFeaturedImage: "images/projects/Vigilante_Page_img.png",
-    projectType: projectType.FRONT_END,
-    projectTechnologiesUsed: [
-      technologiesTypes.JAVASCRIPT,
-      technologiesTypes.HTML,
-      technologiesTypes.CSS,
-    ],
-  },
-  {
-    projectID: 1,
-    projectTitle: "Vigilante 8 2 Page tribute",
-    projectURL: "https://claytonpereira.com/Proj_bloco3_vigilante_page/",
-    projectOverview:
-      " Vigilante 8: 2nd Offense is a vehicular combat game developed by Luxoflux and published by Activision, it was first released in 1999",
-    projectFeaturedImage: "images/projects/Vigilante_Page_img.png",
-    projectType: projectType.FRONT_END,
-    projectTechnologiesUsed: [
-      technologiesTypes.JAVASCRIPT,
-      technologiesTypes.HTML,
-      technologiesTypes.CSS,
-    ],
-  },
-  {
-    projectID: 1,
-    projectTitle: "Vigilante 8 2 Page tribute",
-    projectURL: "https://claytonpereira.com/Proj_bloco3_vigilante_page/",
-    projectOverview:
-      " Vigilante 8: 2nd Offense is a vehicular combat game developed by Luxoflux and published by Activision, it was first released in 1999",
-    projectFeaturedImage: "images/projects/Vigilante_Page_img.png",
-    projectType: projectType.FRONT_END,
-    projectTechnologiesUsed: [
-      technologiesTypes.JAVASCRIPT,
-      technologiesTypes.HTML,
-      technologiesTypes.CSS,
-    ],
-  },
-  {
-    projectID: 1,
-    projectTitle: "Vigilante 8 2 Page tribute",
-    projectURL: "https://claytonpereira.com/Proj_bloco3_vigilante_page/",
-    projectOverview:
-      " Vigilante 8: 2nd Offense is a vehicular combat game developed by Luxoflux and published by Activision, it was first released in 1999",
-    projectFeaturedImage: "images/projects/Vigilante_Page_img.png",
-    projectType: projectType.FRONT_END,
-    projectTechnologiesUsed: [
-      technologiesTypes.JAVASCRIPT,
-      technologiesTypes.HTML,
-      technologiesTypes.CSS,
-    ],
-  },
-];
+state.projects = projectData;
 
 /* Mutating projectsType from state */
 
