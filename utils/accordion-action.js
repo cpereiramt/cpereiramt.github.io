25624e371cd133fd6e2c7f594f06179e9208e5b9
cpereiramt <cpereiramt@gmail.com>
2023-04-22 16:14:07 -0400

    initialization of collapsible

--- utils/accordion-action.js
+++ utils/accordion-action.js
@@ -1,4 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems);
-  });
+  // detected when dom changes in .jw-modal class 
+  var targetNode = document.querySelector('.jw-modal');
+  var config = { attributes: true, childList: true, subtree: true };
+  var callback = function(mutationsList, observer) {
+      if (mutationsList[1]) {
+        var elems = document.querySelectorAll('.collapsible');
+        var instances = M.Collapsible.init(elems, {}); 
+      }
+  };
+  var observer = new MutationObserver(callback);
+  observer.observe(targetNode, config);
+
